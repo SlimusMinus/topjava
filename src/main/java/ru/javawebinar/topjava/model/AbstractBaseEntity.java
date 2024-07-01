7d5d4ae6880de631956dfd2de9d2ec6755bb8f40
Krylov_AY <moscow_city231@rambler.ru>
2024-07-01 23:26:20 +0300

    improve code

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.model;
 
+import org.springframework.util.Assert;
+
 import javax.persistence.*;
 
 @MappedSuperclass
@@ -25,6 +27,11 @@ public abstract class AbstractBaseEntity {
     }
 
     public Integer getId() {
+        return id;
+    }
+
+    public int id() {
+        Assert.notNull(id, "Entity must have id");
         return id;
     }
 
